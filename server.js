08a8e5ab2b7e9fc01f3a33b635ffe84c66b4452c
Mayco Tosco <maycotosco@gmail.com>
2016-09-28 12:35:08 -0300

    Initialize repo

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require ('express');
+var app = express();
+
+/*var middleware = {
+	requireAuthentication: function(req, res, next){
+		conosle.log('fila privada hecha!');
+		next();
+	}
+}*/
+
+//app.use(middleware.requireAuthentication);
+
+app.get('/', function(req, res) {
+	res.send('Hello Mayco!');
+});
+
+app.get('/pagar', function(req, res) {
+	res.send('Dale paga mono!');
+});
+
+app.listen(3000, function(){
+	console.log('Server started!')
+});
