0873c7dbe6cca02d8e025f432b48137b488a1bed
Mayco Tosco <maycotosco@gmail.com>
2016-10-03 11:58:41 -0300

    nuevo intento

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var MP = require ("mercadopago");
 var express = require ('express');
 var app = express();
 
@@ -13,8 +14,48 @@ app.get('/', function(req, res) {
 });
 
 app.get('/pagar', function(req, res) {
-	//res.send('Dale paga monoooo!');
-	var crearPago = require ('./crearPago')
+	res.send('Dale paga monoooo!');
+	//var crearPago = require ('./crearPago')
+});
+
+// POST /todos
+app.post('/renovarSuscripcion', function (req, res) {
+	/*var body = _.pick(req.body, 'description', 'completed');
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();	
+	body.id = todoNextId++;
+
+	todos.push(body);
+	
+	res.json(body);*/
+
+var body = req.body;
+
+var mp = new MP ("TEST-1443215904820832-091611-fb6e772702cb44fa71fbd19024115895__LC_LD__-156286933");
+
+var doPayment = mp.post ("/v1/payments",
+	{
+		"transaction_amount": 100,
+		"token": body.card_token,
+		"description": "Suscripción 90 días",
+		"installments": 1,
+		"payment_method_id": body.payment_method_id,
+		"payer": {
+			"email": "test_user_19653727@testuser.com"
+		}
+	});
+
+doPayment.then (
+	function (payment) {
+		console.log (payment);
+	},
+	function (error){
+		console.log (error);
+	});
 });
 
 app.listen(PORT, function(){
