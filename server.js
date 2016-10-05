61bc8813d775272219570b6b26900eb32cdf7fd2
Mayco Tosco <maycotosco@gmail.com>
2016-10-05 20:20:48 -0300

    usando get

--- server.js
+++ server.js
@@ -10,28 +10,6 @@ var PORT = process.env.PORT || 3000;
 //app.use(middleware.requireAuthentication);
 
 app.get('/', function(req, res) {
-	res.send('Hello Mayco!');
-});
-
-app.get('/pagar', function(req, res) {
-	res.send('Dale paga monoo!');
-	//var crearPago = require ('./crearPago')
-});
-
-// POST /todos
-app.post('/renovarSuscripcion', function (req, res) {
-	/*var body = _.pick(req.body, 'description', 'completed');
-
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
-
-	body.description = body.description.trim();	
-	body.id = todoNextId++;
-
-	todos.push(body);
-	
-	res.json(body);*/
 
 var body = req.body;
 
@@ -40,10 +18,10 @@ var mp = new MP ("TEST-1443215904820832-091611-fb6e772702cb44fa71fbd19024115895_
 var doPayment = mp.post ("/v1/payments",
 	{
 		"transaction_amount": 100,
-		"token": body.card_token,
+		"token": "fb6e772702cb44fa71fbd190241158", //body.card_token
 		"description": "Suscripción 90 días",
 		"installments": 1,
-		"payment_method_id": body.payment_method_id,
+		"payment_method_id": "visa",//body.payment_method_id,
 		"payer": {
 			"email": "test_user_19653727@testuser.com"
 		}
